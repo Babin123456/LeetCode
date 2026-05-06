caeddb6ed4b6d68bfd81b43f51f4fa0fee10a068
KGFCH2 <babinbid05@gmail.com>
2026-05-07 00:40:37 +0530

    1861 Solved

---
+++ 1861. Rotating the Box.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        n, m = len(boxGrid), len(boxGrid[0])
+
+        for i in range(n):
+            empty = m - 1
+            for j in range(m - 1, -1, -1):
+                val = boxGrid[i][j]
+                if val == '*':
+                    empty = j - 1
+                elif val == '#':
+                    boxGrid[i][j] = '.'
+                    boxGrid[i][empty] = '#'
+                    empty -= 1
+
+        res = [[None] * n for _ in range(m)]
+        for i in range(n):
+            for j in range(m):
+                res[j][n - 1 - i] = boxGrid[i][j]
+        return res
