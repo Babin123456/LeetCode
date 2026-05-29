d49483098909e392f7d99a0f3d0e788713db60fe
KGFCH2 <babinbid05@gmail.com>
2026-05-29 07:41:58 +0530

    3300 Solved

---
+++ 3300. Minimum Element After Replacement With Digit Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        min_sum = float('inf')
+        
+        for num in nums:
+            current_sum = 0
+            while num > 0:
+                current_sum += num % 10
+                num //= 10
+            
+            if current_sum < min_sum:
+                min_sum = current_sum
+                
+        return min_sum
